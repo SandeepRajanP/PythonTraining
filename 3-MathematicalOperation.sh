9feca3ba2cd472e14286412372736415735efc43
SandeepRajanP <sandeeprajan.p@quantiphi.com>
2018-06-01 03:32:00 +0530

    3rd assignment problem

---
+++ 3-MathematicalOperation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Input 2 numbers"
+read X
+read Y
+
+#Addition
+echo $(($X+$Y))
+
+#Subtraction
+echo $(($X-$Y))
+
+#Multiplication
+echo $(($X*$Y))
+
+#Division
+echo $(($X/$Y))
+
